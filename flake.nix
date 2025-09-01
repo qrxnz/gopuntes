b7cf31c1079b1a57ba7ce5643d0f3c0acee53dd8
Karol Skoczyk <send@qrxnz.dev>
2025-09-01 11:43:50 +0200

    feat(dev): Set up development environment with Nix and Go
    Add 'flake.nix' to define a reproducible development environment with Go, gopls, and other tools.
    
    Integrate 'direnv' for automatic environment loading upon entering the directory.
    
    Initialize the Go module ('go.mod').
    
    Add a standard '.gitignore' for Go projects and entries for 'direnv'.

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+            # Go
+            go
+            gopls
+            delve
+
+            # Formatters
+            treefmt2
+            mdformat
+            alejandra
+
+            # Others
+            just
+            watchexec
+          ];
+        };
+      }
+    )
+    // {
+      overlays.default = self: pkgs: {
+        hello = self.packages."${pkgs.system}".hello;
+      };
+    };
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gopuntes
+
+go 1.24.6
